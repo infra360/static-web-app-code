a6c9fcbe7f5a84e7c9f4ef03d4841af4c0d24676
abhishekkumar-spec <abhishek.kumar@infra360.io>
2026-01-15 11:40:37 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function changeMessage() {
+  const message = document.getElementById("message");
+
+  const now = new Date();
+  message.innerText = `JavaScript is working! 
+Current time: ${now.toLocaleTimeString()}`;
+}
